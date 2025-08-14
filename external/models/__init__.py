dc19c130329c39dddc2d08bc16856c8c0206ba1e
Cosimo Curri <ccurri001@gmail.com>
2025-08-14 23:43:39 +0200

    Model and dataset cleaning

--- external/models/__init__.py
+++ external/models/__init__.py
@@ -12,11 +12,11 @@ for _backend in sys.modules["external"].backend:
     if _backend == "tensorflow":
         pass
     elif _backend == "pytorch":
-        from .ngcf.NGCF import NGCF
-        from .lightgcn.LightGCN import LightGCN
-        from .dgcf.DGCF import DGCF
-        from .sgl.SGL import SGL
-        from .ultragcn import UltraGCN
-        from .gfcf import GFCF
-        from .sgl import SGL
+        # from .ngcf.NGCF import NGCF
+        # from .lightgcn.LightGCN import LightGCN
+        # from .dgcf.DGCF import DGCF
+        # from .sgl.SGL import SGL
+        # from .ultragcn import UltraGCN
+        # from .gfcf import GFCF
+        # from .sgl import SGL
         from .contextgnn import ContextGNN
--- external/models/dgcf/__init__.py
+++
@@ -1,1 +0,0 @@
-from .DGCF import DGCF
